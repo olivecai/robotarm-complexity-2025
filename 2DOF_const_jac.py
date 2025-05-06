Plot logic fixed: bug was failure to update the Jacobian for each pair of q0,q1. Next steps should be L and R plots, and see if I can make the plot smootha nd not jagged.

2DOF_const_jac.py
@@ -10,7 +10,9 @@ import matplotlib.pyplot as plt
 PI=np.pi
 SIN=np.sin
 COS=np.cos
-DESIRED=np.array([1,1])
+DESIRED=np.array([0,1])
+l0=1 #hardcode lengths of arms
+l1=1
 
 def get_pos(e, theta):
     ''' NOTE: idxing is for SE3 translation/rotation matrix, not 2D!!!'''
@@ -35,18 +37,17 @@ def inverse_kinematics_2d(e, theta, desired_pos, tolerance, maxiter, J): #uses a
         old_pos=get_pos(e, old_theta)
 
         residual = old_pos - desired_pos
-        correction_step = -np.linalg.solve(J, residual)
+        correction_step = -np.linalg.pinv(J) @ residual 
+
         error = norm(residual)
         theta = old_theta + correction_step
 
     return error
 
-
-
 def errorchart(e, desired, tolerance, maxiter, J):
     # make a linspace and meshgrid for ranges of q0 and q1
-    n = 50  #resolution
-    q_range = np.linspace(0, 2*PI, n)
+    n = 100  #resolution
+    q_range = np.linspace(-PI, PI, n )
     Q0, Q1 = np.meshgrid(q_range, q_range) 
     Error = np.zeros_like(Q0)
 
@@ -55,23 +56,25 @@ def errorchart(e, desired, tolerance, maxiter, J):
         for j in range(n):
             q0 = Q0[i, j]
             q1 = Q1[i, j]
+            J = np.array([
+                        [-l0 * SIN(q0) - l1 * SIN(q0 + q1), -l1 * SIN(q0 + q1)],
+                        [ l0 * COS(q0) + l1 * COS(q0 + q1),  l1 * COS(q0 + q1)]
+                        ]);
             Error[i,j] = inverse_kinematics_2d(e, np.array([q0,q1]), desired, tolerance, maxiter, J) #calculate the error of this particular q0 and q1 and find the inverse kinematics
 
-    #cPlot the Error Surface ---
+    #Plot the Error Surface ---
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(Q0, Q1, Error, cmap='plasma')
-    
+
     ax.set_xlabel("q0 (rad)")
     ax.set_ylabel("q1 (rad)")
     ax.set_zlabel("Position Error")
     plt.show()
 
 def main():
-    l0=1 #hardcode lengths of arms
-    l1=1
-    q0=PI #hardcode test angles for now
-    q1=PI
+    q0=1 #hardcode test angles for now
+    q1=1
     theta=np.array([q0,q1]);
     e2 = rtb.ET.Rz() * rtb.ET.tx(l0) * rtb.ET.Rz() * rtb.ET.tx(l1) #each arm is 1m long.
     T= e2.eval(theta)
@@ -91,12 +94,21 @@ def main():
 
     #Inverse kinematics: try to make this code as modular as possible! Make jacobian constant. If the difference between the const_jac pos and the actual pos are increasing after 5 iterations, can we assume it will fail?
     maxiter=30; tolerance=1e-6;
-    '''
+    
     # NOTE: the next two lines were just setting up the inv kin 2D for one theta pair. Now let's go on to iterate over our linspace so we can make a graph 
-    error = inverse_kinematics_2d(e2, theta, DESIRED,tolerance, maxiter, J) #inv kin using const jac
-    print("this is the error:"); print(error)'''
-    errorchart(e2, DESIRED,tolerance, maxiter, J)
+    #error = inverse_kinematics_2d(e2, theta, DESIRED,tolerance, maxiter, J) #inv kin using const jac
+    #print("this is the error:"); print(error)
+    errorchart(e2, DESIRED, tolerance, maxiter, J)
 
+def testing():
+    #see where the angle extends from
+    theta = np.array([PI/2,-PI/2])
+    e2 = rtb.ET.Rz() * rtb.ET.tx(l0) * rtb.ET.Rz() * rtb.ET.tx(l1) #each arm is 1m long.
+    T= e2.eval(theta)
+    print(T)
+
+
+#testing();
 main();
 print("End of program.")
 