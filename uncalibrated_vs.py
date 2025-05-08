8ed36e7862ef6f770148991cb124a877341fb9b9
olivecai <ocai@ualberta.ca>
2025-05-08 17:00:50 -0600

    Jacobian is oscillating... checking for bugs right now.

--- uncalibrated_vs.py
+++ uncalibrated_vs.py
@@ -24,16 +24,33 @@ def fkin3D(e, theta):
     T= e.eval(theta)
     x=T[0][3]; 
     y=T[1][3];
-    z=T[2][3];
+    #z=T[2][3]; #for now to avoid div by zero (THOUGH WE NEED TO FIGURE THAT OUT)
+    z=3;
     return np.array([x,y,z]);
+
+def proj_point(realP, camera: CentralCamera):
+    '''
+    This function only exists because I didn't like the format of the 
+    vision toolbox point projection, which is [[a],[b]] and I want [a,b]
+      
+    Takes in a real world point in SE3 and returns the projection in the x-y plane.
+    '''
+    projP = camera.project_point(realP)
+    posPx = projP[0][0]
+    posPy=projP[1][0]
+    return np.array([posPx, posPy])
+    
 
 def vs_fkin(e,theta, camera: CentralCamera):
     '''
     returns the position of the projection of the EE.  
     '''
     posR = fkin3D(e,theta)
+    print("Real point: ", posR)
     posP = camera.project_point(posR)
-    return posP
+    posPx = posP[0][0]
+    posPy = posP[1][0]
+    return np.array([posPx, posPy])
 
 def init_camera(f, rho, px, py, imagesize) -> CentralCamera:
     return mvtb.CentralCamera(f=f, rho=rho, pp=(px,py), imagesize=imagesize)
@@ -50,12 +67,12 @@ def uncalibrated_vs(currQ, desiredP, camera: CentralCamera, e: rtb.Robot.ets):
     '''
     i=0; 
     currP=vs_fkin(e, currQ, camera) #projection of that real point through the camera
-    errorP=currP-desiredP
+    errorP=desiredP-currP
     error=np.linalg.norm(errorP)
     if error < TOLERANCE:
-        return (i, error currQ) # our ready pose IS the desired pose
+        return (i, error, currP, currQ) # our ready pose IS the desired pose
 
-    epsilon=0.15*math.radians
+    epsilon=math.radians(0.15)
     alpha=0.5
 
     ###sanity check for our dimensions: 
@@ -64,41 +81,50 @@ def uncalibrated_vs(currQ, desiredP, camera: CentralCamera, e: rtb.Robot.ets):
     # DP=kxd, DQ=kxp, currJ.T=pxd... so currJ=dxp. 
     # Also, I is pxp
     # in this case, p=2, d=2
-    p=currQ.shape[1]
+    p=currQ.shape[0]
     d=2 #d is world dimensions
     k=1 #for now its not overdetermined. NOTE: find out if Dylan's paper used overdet.
-    currJT = np.zeros((p,d)) #q=np.array([q0,q1]) for this specific case.
+    currJT = np.zeros((p,d)) 
     DP=np.zeros((k,d))
     DQ=np.zeros((k,p))
     I=np.identity(p)
-    for i in p: #i is a specific robot param
+    for i in range(p): #i is a specific robot param
         #the jacobian is first initialized with central differences. 
+        print("currQ: ")
+        print(currQ)
+        print(I[i])
+        print(vs_fkin(e, currQ+epsilon*I[i], camera))
+
         currJT[i] = ((vs_fkin(e, currQ+epsilon*I[i], camera))-(vs_fkin(e, currQ-epsilon*I[i], camera)))/2*epsilon
-        currJ=currJT.T
-        # NOTE: advised to make f: actual theta -> result image position. double check if mine is OK! :>
+    currJ=currJT.T
+    print("currJ:")
+    print(currJ)
+    # NOTE: advised to make f: actual theta -> result image position. double check if mine is OK! :>
     ###now that we have a Jacobian estimate, we can solve for the correction amt in Q.
     corrQ = np.linalg.pinv(currJ) @ errorP
     currQ=currQ+corrQ
     ###after solvign for the new theta, we move there and check the error.
     currP=vs_fkin(e, currQ, camera)
-    errorP=currP-desiredP
+    errorP=desiredP-currP
     error=np.linalg.norm(errorP)
     i+=1
 
     ### after the initial setup and estimating J, we begin the iterations: 
-    maxiter=50;
+    maxiter=100;
     while i< maxiter and error>TOLERANCE:
         #update J using Broyden's method:
         currJ = currJ + alpha* ((errorP.T - currJ@corrQ)@corrQ.T)/(corrQ.T@corrQ)
+        print("currJ:")
+        print(currJ) 
         corrQ = np.linalg.pinv(currJ) @ errorP #calculate the change in theta needed
         currQ=currQ+corrQ #update the current theta 
         #use the current theta to find our new position and error:
         currP=vs_fkin(e, currQ, camera)
-        errorP=currP-desiredP
+        errorP=desiredP-currP
         error=np.linalg.norm(errorP)
         i+=1;
     ### return i, error, pos
-    return i, error, currP        
+    return i, error, currP, currQ      
 
 def main():
     #CAMERA PARAMS
@@ -111,13 +137,19 @@ def main():
     l0=1
     l1=1
 
-    initialQ = np.array([PI/2, PI/2]) #ready position of the robot
-    desiredP = np.array([1,1,0])
-    camera = init_camera(f, rho, u0, v0, 1000)
+    camera : CentralCamera = init_camera(f, rho, u0, v0, 1000)
+
+    initialQ = np.array([PI/2, -PI/2]) #ready position of the robot
+    desiredRP = np.array([0,2,3])
+    desiredPP = proj_point(desiredRP, camera)
 
     #just use ets. but if we want to use a robot, we can simply transform the robot into an ets
     ets = rtb.ET.Rz() * rtb.ET.tx(l0) * rtb.ET.Rz() * rtb.ET.tx(l1) #each arm is 1m long.
 
-    iterations, error, position = uncalibrated_vs(initialQ, desiredP, camera, ets)
+    iterations, error, position, theta = uncalibrated_vs(initialQ, desiredPP, camera, ets)
+    print("iterations:", iterations,"error:", error, "resulting projected position:", position)
+    print("real pos of the resultant projection:",fkin3D(ets, theta))
+    print("desired real pos:", desiredRP, "desired projected pos:", desiredPP)
 
+main()
 
