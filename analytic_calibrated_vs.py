326cb146ccf41fa119e0280b6fdf204b5b2b0ebd
olivecai <ocai@ualberta.ca>
2025-06-13 14:51:05 -0600

    next up is getting the spectral radius, but we ar erunning into problems with the dimensions of our matrix

--- analytic_calibrated_vs.py
+++ analytic_calibrated_vs.py
@@ -21,17 +21,10 @@ import common_robot_calculations as crc
 # for a desired position, what is the analytic Newton's equation?
 
 # u and v <==> theta1 and theta2
-u = sp.Symbol('u', real=True)
-v = sp.Symbol('v', real=True)
-# l1 and l2 
-l1 = sp.Symbol('l1', positive=True)
-l2 = sp.Symbol('l2', positive=True)
-l1=1;l2=1; #for ease let's just have lengths be 1
+u,v,w = sp.symbols('u:w', real=True)
 
-#declare the forward kin functions
-f1 = l1 * sp.cos(u) + l2 * sp.cos(u+v)  # x coord
-f2 = l1 * sp.sin(u) + l2 * sp.sin(u+v)  # y coord
-f3 = 0 #z coord, robot lies on x-y plane at origin
+# l1 and l2 
+l1,l2,l3 = sp.symbols('l(1:4)', positive=True)
 
 #CAMERA 
 fx = sp.Symbol('fx', real=True)
@@ -43,9 +36,7 @@ rotation_symbols= np.array(sp.symbols('r1(1:4), r2(1:4), r3(1:4)'))
 R = sp.Matrix([rotation_symbols[0:3], rotation_symbols[3:6], rotation_symbols[6:9]]) #over parameterized 
 t = sp.Matrix(sp.symbols('t1(1:4)'))
 
-E = R.col_insert(3, t) #extrinsic matrix
-#TODO: Add the homog row at bottom
-
+E = R.col_insert(3, t).row_insert(3, sp.Matrix([[0,0,0,1]])) #extrinsic matrix
 
 print(R)
 print(t)
@@ -69,6 +60,13 @@ print(x[1])
 print(x[2])
 
 def transformation_matrix_DH(theta_i, alpha_i, r_i, d_i):
+    '''
+    Returns the general denavit hartenberg transformation matrix for one link, i.
+    theta_i is the angle about the z(i-1) axis between x(i-1) and x(i).
+    alpha_i is the angle about the x(i) axis between z(i-1) and z(i).
+    r_i is the distance between the origin of frame i-1 and i along the x(i) direction.
+    d_i is the distance from x(i-1) to x(i) along the z(i-1) direction.
+    '''
     alpha, r, theta, d = sp.symbols('alpha, r, theta, d', real=True)
     general_DH = sp.Matrix([[sp.cos(theta), -sp.sin(theta)*sp.cos(alpha), sp.sin(theta)*sp.sin(alpha), r*sp.cos(theta)],
                             [sp.sin(theta), sp.cos(theta)*sp.cos(alpha), -sp.cos(theta)*sp.sin(alpha), r*sp.sin(theta)],
@@ -90,10 +88,13 @@ def denavit_hartenburg_dylan_3dof(t0, t1, t2):
      [ t1,  0  ,  0.55, 0 ],
      [ t2,  0  ,  0.30, 0 ]]
    
+    Returns an array of positions for plotting, and the final DH transform matrix of the EE.
     '''
+    l1 = 0.55
+    l2 = 0.30
     T01= transformation_matrix_DH(t0, sp.pi/2, 0, 0)
-    T12= transformation_matrix_DH(t1, 0, 2, 0)
-    T23= transformation_matrix_DH(t2, 0, 1, 0)
+    T12= transformation_matrix_DH(t1, 0, l1, 0)
+    T23= transformation_matrix_DH(t2, 0, l2, 0)
     transforms = [T01, T12, T23]
 
     EE = T01 * T12 * T23
@@ -108,12 +109,17 @@ def denavit_hartenburg_dylan_3dof(t0, t1, t2):
         print(pos)
         positions.append(pos)
 
-    print(positions)
+    #print(positions)
 
-    return np.array(positions) 
+    return sp.Matrix(positions), EE
     
 
 def wireframe(joints):
+    '''
+    This function serves to plot the wireframe of our robot points.
+    Input is a series of points, which will be connected in the plot sequentially.
+    Fixed axis limits prevent automatic caling perspective.
+    '''
     print(joints)
     x = joints[:,0]
     y = joints[:,1]
@@ -122,17 +128,20 @@ def wireframe(joints):
     fig=plt.figure()
     ax = fig.add_subplot(projection='3d')
     ax.plot(x,y,z, marker='o', linestyle='-')
+
+    xlim=[-1,1]
+    ylim=[-1,1]
+    zlim=[-1,1]
+    # Set fixed axis limits
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_zlim(zlim)
+
     ax.set_xlabel('X Axis')
     ax.set_ylabel('Y Axis')
     ax.set_zlabel('Z Axis')
     ax.set_title('3D Line Plot')
     plt.show()
-
-joints=denavit_hartenburg_dylan_3dof(0,0,sp.pi/2)
-wireframe(joints)
-
-    
-
 
 def compute_analytic_jac(Q):
     '''
@@ -146,7 +155,7 @@ def compute_analytic_jac(Q):
     And then get the Jacobian of x, as dx
     '''
     x = P * X
-    dx = x.jacobian([u,v])
+    dx = x.jacobian([u,v,w])
     dx=dx.subs([(u, Q[0]), (v, Q[1])])
     print(dx)
     print(sp.shape(dx))
@@ -199,19 +208,45 @@ Robotics textbook:
 - Craig from 464 (has DH params)
 '''
 
-#Just to verify everything is okay, I have the rtb DH robot initialized here.
+def rtb_model():
+    '''
+    Just to verify everything is okay, I have the rtb DH robot initialized here.
+    '''
 
-from roboticstoolbox import DHRobot, RevoluteDH
-from math import pi
+    from roboticstoolbox import DHRobot, RevoluteDH
+    from math import pi
 
-# Define the 3 links using standard DH parameters
-link1 = RevoluteDH(alpha=pi/2, a=0, d=0)
-link2 = RevoluteDH(alpha=0,    a=2, d=0)
-link3 = RevoluteDH(alpha=0,    a=1, d=0)
+    # Define the 3 links using standard DH parameters
+    link1 = RevoluteDH(alpha=pi/2, a=0, d=0)
+    link2 = RevoluteDH(alpha=0,    a=2, d=0)
+    link3 = RevoluteDH(alpha=0,    a=1, d=0)
 
-# Create the robot
-robot = DHRobot([link1, link2, link3], name='3DOF_Robot')
-print(robot.dhunique())
-# Print the robot description
-print(robot)
-robot.plot([0,0,sp.pi/2], block=True)#'''
+    # Create the robot
+    robot = DHRobot([link1, link2, link3], name='3DOF_Robot')
+    print(robot.dhunique())
+    # Print the robot description
+    print(robot)
+    robot.plot([0,0,sp.pi/2], block=True) #'''
+
+
+def main():
+    joints, EE =denavit_hartenburg_dylan_3dof(u,v,w)
+    print("EE:\n",EE)
+    position = ((EE[:,3]).T)
+    print(position)
+    u_des,v_des,w_des = [0,sp.pi/2,sp.pi/2]
+    reps_des=[(u,u_des), (v,v_des), (w, w_des)]
+    #wireframe(joints.subs(reps_des))
+    #print(position.subs(reps_des))
+
+    #position is our analytical cartesian world point!
+
+
+main()
+'''
+Now we have the end effector of the robot and we also have the camera. 
+If we only care about translation and not rotation, 
+we can simply multiply our EE world point by our camera and get the projected point.
+
+With this 
+'''
