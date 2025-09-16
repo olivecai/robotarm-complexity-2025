c3b0c5ad7aa32c4b234051f90930aebbb994c694
oliveoil <ocai@ualberta.ca>
2025-09-16 17:16:37 -0600

    plots finalized

--- simulations/run_vs.sh
+++ simulations/run_vs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo >  BASHAUTOMATION_VS.txt #clear the file
-for robot in dof2 dof3 jaco kinova
+echo >     BASHAUTOMATION_VS9.txt #clear the file
+for robot in dof2 dof3
 do
-echo "robot: ${robot}" >>  BASHAUTOMATION_VS.txt
+echo "robot: ${robot}" >>     BASHAUTOMATION_VS9.txt
 echo "robot: ${robot}"
-for i in {1..5}
+for i in {1..70}
 do
     echo "generating batch" $i
     python3 LLS_traj_vs_random.py ${robot} > temp.txt
-    tail -n 1 temp.txt >>  BASHAUTOMATION_VS.txt
+    tail -n 1 temp.txt >>     BASHAUTOMATION_VS9.txt
     done
 done
 
